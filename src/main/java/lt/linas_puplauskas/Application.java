4708ef9601f441bc0fa2d62b51b562e1d6db6295
Linas <Linas.puplauskas@gmail.com>
2026-04-10 11:18:42 +0200

    first commit

---
+++ src/main/java/lt/linas_puplauskas/Application.java
@@ -0,0 +1,19 @@
+package lt.linas_puplauskas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@SpringBootApplication
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet("styles.css")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/lt/linas_puplauskas/MainView.java
@@ -0,0 +1,31 @@
+package lt.linas_puplauskas;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView(GreetService service) {
+
+        TextField textField = new TextField("Your name");
+        textField.addClassName("bordered");
+
+        Button button = new Button("Say hello", e -> {
+            add(new Paragraph(service.greet(textField.getValue())));
+        });
+
+        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        button.addClickShortcut(Key.ENTER);
+
+        addClassName("centered-content");
+
+        add(textField, button);
+    }
+}
